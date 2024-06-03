f301380f7afe783c4de62cd9efe41f01092f4038
iqbaldf7 <Iqbaldathonah10@gmail.com>
2024-06-03 14:54:03 +0700

    unlimited loop untuk menulis,mendapatkan karakter dalam satu baris,loop akan berhenti jika memasukan karakter berupa q

--- iofilewithpath.cpp
+++ iofilewithpath.cpp
@@ -17,8 +17,27 @@ int main() {
 
     cout << ">= Menulis file, \'q\' untuk keluar" << endl;
 
+
     //unlimited loop untuk menulis
     while (true) {
         cout << "-";
         //mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        //loop akan berhenti jika anda memasukan karakter q
+        if (baris == "q") break;
+        //menulis dan memasukan nilai dari 'baris' ke dalam fle
+        outfile << baris << endl;
+
     }
+    
+    //selesai dalam menulis sekarang tutup filenya
+    outfile.close();
+
+    //Membuka file dalam mode membaca
+    ifstream infile;
+
+    //menunjuk ke sebuah file
+    infile.open(NamaFile + ".txt", ios::in);
+
+    cout << endl << ">= Membuka dan membaca file " << endl;
+    //jika file ada maka
