e5c6df5328c90754fdea68a7f6ba4cc2f2719ca4
iqbaldf7 <Iqbaldathonah10@gmail.com>
2024-06-03 15:24:26 +0700

    untuk mengatasi eror

--- manual_exception.cpp
+++ manual_exception.cpp
@@ -1,2 +1,22 @@
-#include <isotream>
+#include <iostream>
 using namespace std;
+
+
+int main() 
+{
+    try {
+        cout << "selamat belajar di prodi TI UMY" << endl;
+        //throw 0.5; //melemparkan sebuah integer maka
+        cout << "pernyataan tidak akan dieksekusi" << endl;
+    }
+    catch (int a) {
+        //blok ini akan dieksekusi
+        cout << "pernyataan akan dieksekusi" << endl;
+    }
+    catch(...) {
+        cout << "default pengecualian dieksekusi" << endl;
+    }
+    return 0
+
+}
+
