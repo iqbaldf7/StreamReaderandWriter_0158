aa5074bc6ee016a5e0f2d5c67d0b852d1e737a47
iqbaldf7 <Iqbaldathonah10@gmail.com>
2024-06-03 14:47:08 +0700

    memasukan file, membuka file dalam mode menulis,menunjuk ke sebuah nama file

--- iofilewithpath.cpp
+++ iofilewithpath.cpp
@@ -2,3 +2,23 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+int main() {
+    string baris;
+    string NamaFile;
+
+    cout << "Masukan Nama File : ";
+    cin >> NamaFile;
+
+    //Membuka file dalam mode menulis
+    ofstream outfile;
+    //menunjuk ke sebuah nama file
+    outfile.open(NamaFile + ".txt", ios::out);
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+    //unlimited loop untuk menulis
+    while (true) {
+        cout << "-";
+        //mendapatkan setiap karakter dalam satu baris
+    }
