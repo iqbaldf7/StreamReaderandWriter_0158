bc227648f3be0daf3456fc02e9918374a5ffee21
iqbaldf7 <Iqbaldathonah10@gmail.com>
2024-06-03 15:00:18 +0700

    membuka dan membaca file,melakukan perulangan setiap baris, tutup file setelah selesai

--- iofilewithpath.cpp
+++ iofilewithpath.cpp
@@ -37,10 +37,26 @@ int main() {
 
     //Membuka file dalam mode membaca
     ifstream infile;
-    
+
 
     //menunjuk ke sebuah file
     infile.open(NamaFile + ".txt", ios::in);
 
-    cout << endl << ">= Membuka dan membaca file " << endl;
+   cout << endl << ">= membuka dan membaca file " << endl;
     //jika file ada maka
+    if (infile.is_open()) 
+    {
+        //melakukan perulangan setiap baris
+        while (getline(infile, baris)) 
+        {
+            cout << baris << '\n';
+        }
+        //tutup file tersebut setelah selesai
+        infile.close();
+    }
+
+    //jika tidak ditemukan file maka akan menampilkan ini
+    else cout << "unable to open file";
+    return 0;
+
+}
